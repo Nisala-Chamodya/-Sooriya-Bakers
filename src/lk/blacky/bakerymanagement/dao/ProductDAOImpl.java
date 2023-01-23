bd72a9d68c4dae35c572f06426e4e4e389f09bac
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-01-23 22:56:32 +0530

    ItemDAOImpl

---
+++ src/lk/blacky/bakerymanagement/dao/ProductDAOImpl.java
@@ -0,0 +1,46 @@
+package lk.blacky.bakerymanagement.model;
+
+import lk.blacky.bakerymanagement.to.Customer;
+import lk.blacky.bakerymanagement.to.Product;
+import lk.blacky.bakerymanagement.util.CRUDUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductModel {
+    public static boolean AddProduct(Product product) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO product VALUES (?, ?, ?, ?,?)";
+        return CRUDUtil.execute(sql, product.getProductId(),
+                product.getProductName(), product.getPrice(), product.getDescription(), product.getAvailability());
+    }
+
+    public static Product searchProduct(String ProductId) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM product WHERE  product_id=? ";
+        ResultSet resultSet = CRUDUtil.execute(sql, ProductId);
+        if (resultSet.next()) {
+            return new Product(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getDouble(3),
+                    resultSet.getString(4),
+                    resultSet.getInt(5) );
+
+
+
+
+
+        }
+        return null;
+    }
+
+    public static boolean updateProduct(Product product) throws SQLException, ClassNotFoundException {
+        String sql = "UPDATE product SET name=?,price=?,discription=?,availability=? WHERE  product_id=? ";
+        return CRUDUtil.execute(sql, product.getProductName(), product.getPrice(), product.getDescription(), product.getAvailability(), product.getProductId());
+
+    }
+
+    public static boolean deleteProduct(String productId) throws SQLException, ClassNotFoundException {
+        String sql = "DELETE FROM product WHERE product_id=? ";
+        return CRUDUtil.execute(sql, productId);
+    }
+}
